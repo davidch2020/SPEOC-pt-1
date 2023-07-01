2e9ce0aa65839954b41a05b85045aa8ef3c2df55
MariaD18 <107698123+MariaD18@users.noreply.github.com>
2023-06-30 20:45:33 -0400

    Update web_app.py

--- web_app/web_app.py
+++ web_app/web_app.py
@@ -507,14 +507,13 @@ def handle_state_dropdown(state, county, option, map_type):
                                     hover_name="County",
                                     hover_data=["Population"]
                                 )
-            slider =  dcc.RangeSlider(county_pops["Population"].min(), 
-                                      county_pops["Population"].max(), 
+            slider =  dcc.RangeSlider(min = county_pops["Population"].min(), 
+                                      max = county_pops["Population"].max(), 
                                       step= 10,000, 
                                       marks = rangeslider_marks,
                                       value=[county_pops["Population"].min(), county_pops["Population"].max()],
                                       updatemode = 'drag',
-                                      id = "my-rangeslider"
-            )
+                                      id = "my-rangeslider")
         elif map_type == 'slavery':
             
             basemap_visible = True
