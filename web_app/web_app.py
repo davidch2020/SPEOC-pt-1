bdeec09ba22ccf866deee8521e19254c62289ada
MariaD18 <107698123+MariaD18@users.noreply.github.com>
2023-06-30 21:07:18 -0400

    Update web_app.py

--- web_app/web_app.py
+++ web_app/web_app.py
@@ -576,6 +576,11 @@ def handle_state_dropdown(state, county, option, map_type):
             slider =  dcc.RangeSlider(0, 20, value=[5, 15])
 
         return dcc.Graph(figure = fig, id = "my-map"), slider
+        
+        @app.callback(
+                Output('my-map', 'figure'),
+                [Input('my-rangeslider', 'value')]
+        )
     else: # option is table
         # Display the DataFrame as a table
         df = pd.read_csv('../data_clean/final_data_CD.csv', index_col=0)
@@ -594,10 +599,6 @@ def handle_state_dropdown(state, county, option, map_type):
                 'fontWeight': 'bold'
             }
         )
-@app.callback(
-    Output('my-map', 'figure'),
-    [Input('my-rangeslider', 'value')]
-)
 # Layout of the app
 app.layout = html.Div(className='app-container', children=[
     dbc.Container(className='header-container', children=[
