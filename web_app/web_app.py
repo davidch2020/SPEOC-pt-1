a124cf4377f92ffa49d633f32152fbc5650d7e43
MariaD18 <107698123+MariaD18@users.noreply.github.com>
2023-07-08 19:04:48 -0400

    Update web_app.py

--- web_app/web_app.py
+++ web_app/web_app.py
@@ -101,7 +101,7 @@ states_drp = dcc.Dropdown(
 regions_drop = dcc.Dropdown(
     id = "reg_drpdwn",
     options=['Nation','State','County'],
-    value='Nation'
+    value='Choose a Region'
 )
 
 # title : "Pick a state"
