71cbaf496d2520d8ccc3987f020db9d8a50beee9
MariaD18 <107698123+MariaD18@users.noreply.github.com>
2023-07-12 14:07:03 -0400

    Update web_app.py

--- web_app/web_app.py
+++ web_app/web_app.py
@@ -652,7 +652,7 @@ def handle_state_dropdown(state, county, option, map_type, border_type):
             state_pop = state_pop.astype({"Slave Pop":"int"})
 
             fig = px.choropleth(state_pop, geojson=map_gj, locations='State', 
-                            #locationmode='USA-states', 
+                            locationmode='USA-states', 
                             color='Slave Pop',
                             color_continuous_scale="Viridis",
                             range_color=(state_pop['Slave Pop'].min(), 
