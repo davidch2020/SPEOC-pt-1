5c704e801084a202eb4ba39c9d487fb3bfbf8adb
MariaD18 <107698123+MariaD18@users.noreply.github.com>
2023-07-20 13:31:12 -0400

    Update web_app.py

--- web_app/web_app.py
+++ web_app/web_app.py
@@ -644,7 +644,7 @@ def handle_state_dropdown(state, county, option, map_type, border_type, sliderra
             county_pops = county_pops.astype({"SE_T001_001":"int", "Geo_FIPS":"str"})
             county_pops.rename(columns = {'SE_T001_001':'Population', "Geo_name":"County"}, inplace = True)
             county_pops = county_pops[["Geo_FIPS", "Population", "County"]]
-            dcc.Store(data = county_pops, id = 'county_data')
+            #dcc.Store(data = county_pops, id = 'county_data')
 
             #state pop
             state_pops = gpd.read_file("../data_raw/census_data/statepop.csv")
