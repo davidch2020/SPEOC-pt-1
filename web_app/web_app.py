fa9634435c2a76d2dbd45e86b3c55643a79111ac
MariaD18 <107698123+MariaD18@users.noreply.github.com>
2023-07-31 16:05:48 -0400

    Update web_app.py

--- web_app/web_app.py
+++ web_app/web_app.py
@@ -479,23 +479,21 @@ def update_project_desc(left_clicks, right_clicks):
 )
 def display_state_drpdwn(value):
     if value == "State":
-        state_drpdwn_title = html.H5(children="Pick a State", id="state_drpdwn_t", style = {"float": "right"}) #"margin-left": "200px"
+        state_drpdwn_title = html.H5(children="Pick a State", id="state_drpdwn_t", style = {"margin-left": "200px"})
         state_drp = dcc.Dropdown(
             id="states_drpdwn",
             options=states,
             value=states[0],
-            align_end=True,
-            style = {'width': '70%'}#{'width': '70%', "margin-left": "100px"} 
+            style = {'width': '70%', "margin-left": "100px"}
         )
         return state_drpdwn_title, state_drp 
     if value == "County":
-        state_drpdwn_title = html.H5(children="State of Your County", id="state_drpdwn_t", style = {"float": "right"}) #"margin-left": "200px"
+        state_drpdwn_title = html.H5(children="State of Your County", id="state_drpdwn_t", style = {"margin-left": "200px"}) 
         state_drp = dcc.Dropdown(
             id="states_drpdwn",
             options=states,
             value=states[0],
-            align_end=True,
-            style = {'width': '70%'} #{'width': '70%', "margin-left": "100px"}
+            style = {'width': '70%', "margin-left": "100px"} 
         )
         return state_drpdwn_title, state_drp 
     else:
@@ -513,13 +511,12 @@ def display_county_drpdwn(state_value, reg_value):
     if (state_value != "All States") and (state_value is not None):
         counties = map_df.query("state==" + "'" + state_value + "'")["county"].tolist()
         counties.insert(0, "All Counties")
-        county_drpdwn_title = html.H5(children="Choose a County", id="county_drpdwn_t", style = {"float": "right"}) #"margin-left": "200px"
+        county_drpdwn_title = html.H5(children="Choose a County", id="county_drpdwn_t", style = {"margin-left": "200px"})
         county_drp = dcc.Dropdown(
             id="county_drpdwn",
             options=counties,
             value=counties[0],
-            align_end=True,
-            style = {'width': '70%'} #"margin-left": "100px"
+            style = {'width': '70%', "margin-left": "100px"} 
         )
         return county_drpdwn_title, county_drp 
     else:
